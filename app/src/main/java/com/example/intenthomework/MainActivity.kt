c8d251cc50fa11a2c5b4be1e8324d2006cfe18c8
Kirill Cherkas <kirill.cherkas34@gmail.com>
2022-02-20 15:59:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/intenthomework/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.intenthomework
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        btn.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    1
+                )
+            }
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("geo:${location?.latitude.toString()}, ${location?.longitude.toString()}")
+                )
+                startActivity(intent)
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
